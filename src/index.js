138344f21d8e0841156a3ba46a70693aded91c2e
paigerichards <paige.richards14@hotmail.co.uk>
2017-06-26 13:50:07 +1000

    Adding click alert

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import './index.css';
 class Square extends React.Component {
   render(){
     return(
-      <button className='square'>
-        {/* {TODO} */}
+      <button className='square' onClick={() => alert('click')}>
+        {this.props.value}
       </button>
     )
   }
@@ -17,7 +17,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i){
-    return <Square />
+    return <Square value={i} />
   }
   render(){
     const status = 'Next Player: X'
