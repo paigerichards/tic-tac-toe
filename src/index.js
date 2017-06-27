4b7f26af0c14e1796bdf46f71c57055360cf9fd9
paigerichards <paige.richards14@hotmail.co.uk>
2017-06-27 14:21:05 +1000

    Adding minimal css styling

--- src/index.js
+++ src/index.js
@@ -126,7 +126,6 @@ class Game extends React.Component {
 
 // ========================================
 
-ReactDOM.render(<Game />, document.getElementById("root"));
 
 function calculateWinner(squares) {
   const lines = [
